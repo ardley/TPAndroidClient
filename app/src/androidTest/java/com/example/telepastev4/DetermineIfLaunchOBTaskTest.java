47868dc9dd712638a278ede408f5cf847c2c2c3b
Sean <sean.ardley@gmail.com>
2016-09-12 21:51:50 -0700

    updated gradle version to something older so depends would resolve

---
+++ app/src/androidTest/java/com/example/telepastev4/DetermineIfLaunchOBTaskTest.java
@@ -0,0 +1,23 @@
+package com.example.telepastev4;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Sean on 9/12/2016.
+ */
+public class DetermineIfLaunchOBTaskTest extends TestCase {
+
+    Context myMockContext;
+
+    public void testDoInBackground() throws Exception {
+
+        LaunchBR myLaunchBR = new LaunchBR();
+
+
+        assertTrue(myLaunchBR != null);
+    }
+
+}
